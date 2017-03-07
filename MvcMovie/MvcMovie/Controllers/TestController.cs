33ce4878058968bea56e263e3abc47e1d274dcf5
mukykuts <mukykuts@MUKW0696>
2017-03-07 15:56:21 +0200

    YooouHooo)

---
+++ MvcMovie/MvcMovie/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MvcMovie.Controllers
+{
+    public class TestController : Controller
+    {
+     
+       
+
+        public ActionResult Index()
+        {
+            NorthwindEntities entities = new NorthwindEntities();
+            return View(entities.SearchCustomers(""));
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(string customerName)
+        {
+            NorthwindEntities entities = new NorthwindEntities();
+            return View(entities.SearchCustomers(customerName));
+        }
+    }
+}
---
+++ TestInfo/TestInfo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestInfo.MvcApplication" Language="C#" %>
